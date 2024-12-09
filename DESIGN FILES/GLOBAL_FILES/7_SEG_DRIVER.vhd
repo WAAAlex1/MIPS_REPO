98d89241cffcf82e690b2c1c4529cdae8fa6cbe6
WAAAlex1 <Alexaakerso@gmail.com>
2024-12-09 11:27:38 -0500

    7-SEGMENT FIXED
    did so much work for such a small mistake

--- DESIGN FILES/GLOBAL_FILES/7_SEG_DRIVER.vhd
+++ DESIGN FILES/GLOBAL_FILES/7_SEG_DRIVER.vhd
@@ -6,25 +6,26 @@ entity SEVEN_SEG_DRIVER is
 	port(
 		clk       : in  std_logic;
 		data      : in  std_logic_vector(15 downto 0);
-		ano       : out std_logic_vector(4 downto 1);
-		seg_out   : out std_logic_vector(7 downto 1)
+		ano       : out std_logic_vector(3 downto 0);
+		seg_out   : out std_logic_vector(6 downto 0)
 	);
 end SEVEN_SEG_DRIVER;
 
 architecture arch of SEVEN_SEG_DRIVER is
 	
 	signal cnt: unsigned(18 downto 0) := to_unsigned(0,19); --Need 19 bits to count to 300.000 (3ms uptime for each anode)
-	signal ano_1: std_logic_vector(ano'high downto ano'low):=b"1000";
+	signal ano_1: std_logic_vector(ano'high downto ano'low):=b"0001";
 	constant num_bits: natural:=19;
-	constant count_target: natural:=300000;
-	
-	signal index: unsigned(1 downto 0):=b"00";
+	constant count_target: natural:=300000; --300000
 	
 	type DATA_T is ARRAY(3 downto 0) of STD_LOGIC_VECTOR(3 DOWNTO 0);
 	signal DATA_IN: DATA_T;
 	
 	type PATTERN_T is ARRAY(3 downto 0) of STD_LOGIC_VECTOR(7 DOWNTO 1);
 	signal pattern: PATTERN_T;
+	
+	type FSM_TYPE is (STATE0,STATE1,STATE2,STATE3);
+	signal FSM: FSM_TYPE:=STATE0;
 	
 begin
     -- INPUTS
@@ -63,22 +64,52 @@ begin
 	-- use simple counter. When count reached circular shift anode right once. 
 	-- as each digit should then be turned on for 3ms -> 3/1000hZ -> needs 300.000 cycles from 100MHz clock. 
 	
-	process(clk, cnt, index, ano_1) 
+	process(clk, FSM, pattern, cnt) 
 	begin
 	if rising_edge(clk) then
-	   if cnt = to_unsigned(count_target,num_bits) then
-           ano_1 <= ano_1(ano_1'low)&ano_1(ano_1'high downto ano_1'low+1); -- Right circular shifting
-           cnt <= to_unsigned(0,num_bits);
-           seg_out <= not pattern(to_integer(index));
-           if index = 3 then
-              index <= to_unsigned(0,2);
-           else
-              index <= index+1;
-           end if;      
-       else
-           cnt <= cnt+1;
-       end if;
-	 end if;
+	   case FSM is
+	       when STATE0=>
+               seg_out <= not pattern(0);
+               ano_1 <= "1110";
+               if(cnt = count_target) then
+                   FSM <= STATE1;
+                   cnt <= to_unsigned(0,num_bits);
+               else
+                   FSM <= FSM;  
+                   cnt <= cnt+1;  
+               end if;
+	       when STATE1=>
+               seg_out <= not pattern(1);
+               ano_1 <= "1101";
+               if(cnt = count_target) then
+                   FSM <= STATE2;
+                   cnt <= to_unsigned(0,num_bits);
+               else
+                   FSM <= FSM;  
+                   cnt <= cnt+1;  
+               end if;
+	       when STATE2=>
+               seg_out <= not pattern(2);
+               ano_1 <= "1011";
+               if(cnt = count_target) then
+                   FSM <= STATE3;
+                   cnt <= to_unsigned(0,num_bits);
+               else
+                   FSM <= FSM;  
+                   cnt <= cnt+1;  
+               end if;
+	       when STATE3=>
+               seg_out <= not pattern(3);
+               ano_1 <= "0111";
+               if(cnt = count_target) then
+                   FSM <= STATE0;
+                   cnt <= to_unsigned(0,num_bits);
+               else
+                   FSM <= FSM;  
+                   cnt <= cnt+1;  
+               end if;
+	       end case;
+	   end if;      
 	end process;
 
 end arch;
